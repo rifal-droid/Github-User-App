4567396a861611419988afdfbd3e26fd9f30b8a6
Teuku Rifal Aulia <60639687+rifal-droid@users.noreply.github.com>
2023-10-18 20:12:59 +0700

    Update Api.kt
    menghapus API github dari app

--- GitHubUserSearch_app-master/app/src/main/java/com/rifal/myapplication/api/Api.kt
+++ GitHubUserSearch_app-master/app/src/main/java/com/rifal/myapplication/api/Api.kt
@@ -11,28 +11,28 @@ import retrofit2.http.Query
 
 interface Api {
     @GET("search/users")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token [token github] ")
 
     fun getSearchUsers(
         @Query("q") query:String
     ): Call<UserResponse>
 
     @GET("users/{username}")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token [token github]")
 
     fun getDetailUsers(
         @Path("username") username : String
     ):Call<UserDetailResponse>
 
     @GET("users/{username}/followers")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token [token github]")
 
     fun getFollowers(
         @Path("username") username : String
     ):Call<ArrayList<User>>
 
     @GET("users/{username}/following")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token [token github]")
 
     fun getFollowing(
         @Path("username") username : String
